b2f32719158f0eca00940edeae6d26e37fdad01b
Nicholas Indy Ray <indy@ishere.com>
2009-09-15 12:53:03 -0700

    C++ virtual method callback support, constructor callback functional.
    * Updated qt-proxy-class to create callback functions for the constructor, destructor, and method list.
    * Updated qt-proxy-class constructor generation to call the constructor callback.
    * Updated test qt-proxy-class call to print messages on callback.
    * Removed old unused defines.
    * Removed rename paramater from the make-name func.
    * Added map-names to better replace param-list.
    * Created symbol-append to create a new symbol as the summation of a list of sumbols.
    * Added qt-proxy-callback and qt-proxy-ballback-name That work to provide callbacks for C++ virtual methods into scheme.

--- qt.scm
+++ qt.scm
@@ -1,10 +1,6 @@
 (declare (emit-external-prototypes-first))
 (require-extension srfi-1)
-
-(define class-name 'ImageWindow)
-(define parent-class 'QMainWindow)
-(define constructor-params '((QWidget* parent 0)))
-(define parent-params '(parent))
+(import-for-syntax matchable)
 
 ;; Macros need hygiene!
 
@@ -51,7 +47,7 @@
 (define qt-class-list (make-hash-table))
 
 (define-for-syntax make-name
-  (lambda (r i)
+  (lambda (i)
     (string->symbol
      (string-append
       "s"
@@ -65,6 +61,52 @@
           (cons
            (func param i)
            (param-list func (cdr params) (+ i 1)))))))
+
+(define-for-syntax map-names
+  (lambda (func params)
+    (param-list
+     (lambda (param i)
+       (func param (make-name i)))
+     params
+     0)))
+
+(define-for-syntax symbol-append
+  (lambda symbol-list
+    (string->symbol
+     (apply
+      string-append
+      (map
+       (lambda (x)
+         (->string x))
+       symbol-list)))))
+
+(define-for-syntax qt-proxy-callback-name
+  (lambda (name)
+    (string->symbol
+     (string-append
+      "C_callback_"
+      (->string name)))))
+
+(define-for-syntax qt-proxy-callback
+  (lambda (callback-name
+      params
+      return
+      callback)
+    `(define-external (,(qt-proxy-callback-name callback-name)
+                       (scheme-object self)
+                       ,@(map-names
+                          (lambda (param name)
+                            (list
+                             (cadr param)
+                             name))
+                          params))
+       ,return
+       (,callback
+        self
+        ,@(map-names
+           (lambda (param name)
+             name)
+           params)))))
 
 (define-syntax qt-foreign-define
   (lambda (e r c)
@@ -113,7 +155,7 @@
             (lambda (param i)
               (list
                param
-               (make-name r i)))
+               (make-name i)))
             params 0))
         ,return
         ,(apply
@@ -133,7 +175,7 @@
                   (if (> i 0)
                       ", "
                       "")
-                  (->string (make-name r i))))
+                  (->string (make-name i))))
                params 0)
             ,(if (eq? return 'void)
                  ""
@@ -170,7 +212,7 @@
                   (param-list
                    (lambda (param i)
                      (if (not (eq? param 'self))
-                         (make-name r i)
+                         (make-name i)
                          #f))
                    constructor-params 0)))
              (letrec ((,(r 'self)
@@ -184,7 +226,7 @@
                                (if (eq? param 'self)
                                    'scheme-object
                                    (cadr param))
-                               (make-name r i)))
+                               (make-name i)))
                             constructor-params 0)
                           ,(apply
                             string-append
@@ -199,15 +241,15 @@
                                          ", ("
                                          (->string (car param))
                                          ")"
-                                         (->string (make-name r i)))
-                                        (make-name r i))))
+                                         (->string (make-name i)))
+                                        (make-name i))))
                                  constructor-params 0)
                               "));")))
                          ,@(param-list
                             (lambda (param i)
                               (if (eq? param 'self)
                                   (r 'self)
-                                  (make-name r i)))
+                                  (make-name i)))
                             constructor-params 0)))))
                ,(r 'self))))
          (qt-foreign-define
@@ -221,60 +263,104 @@
 
 (define-syntax qt-proxy-class
   (lambda (e r c)
-    (let
-        ((class-name (cadr e))
-         (parent-class (caddr e))
-         (constructor (cadddr e))
-         (constructor-params (car (cddddr e)))
-         (parent-params (cadr (cddddr e))))
-      `(begin
-         (foreign-declare
-          ,(apply
-            string-append
-            (append
-             (list "class " (->string class-name) " : public " (->string parent-class)
-                   "{"
-                   "	void* proxy_root;"
-                   "public:"
-                   (->string class-name) "("
-                   "C_word proxy")
-             (map
-              (lambda (param)
-                (string-append
-                 ", "
-                 (->string (caar param))
-                 " "
-                 (->string (cadr param))))
-              constructor-params)
-             (list
-              ") :"
-              (->string parent-class) "(")
-             (map
-              (lambda (params)
-                (->string params))
-              parent-params)
-             (list
-              ")"
-              "{"
-              "	proxy_root = CHICKEN_new_gc_root();"
-              "	CHICKEN_gc_root_set(proxy_root, proxy);"
-              "}"
+    (match
+      e
+      (('qt-proxy-class
+        class-name
+        parent-class
+        (constructor
+          constructor-params
+          constructor-func)
+        parent-params
+        destructor-func
+        proxies)
+       `(begin
+          ;; Constructor callback
+          ,(qt-proxy-callback
+            (symbol-append
+             class-name
+             'constructor)
+            (map
+             (lambda (X)
+               (car X))
+             constructor-params)
+            'void
+            constructor-func)
 
-              "~" (->string class-name) "()"
-              "{"
-              "CHICKEN_delete_gc_root(proxy_root);"
-              "}"
+          ;; Destructor callback
+          ,(qt-proxy-callback
+            (symbol-append
+             class-name
+             'destructor)
+            '()
+            'void
+            destructor-func)
 
-              "};"))))
+          ;; Proxies callbacks
+          ,@(map
+             (lambda (proxy)
+               (apply qt-proxy-callback proxy))
+             proxies)
+          (foreign-declare
+           ,(apply
+             string-append
+             (append
+              (list "class " (->string class-name) " : public " (->string parent-class)
+                    "{"
+                    "	void* proxy_root;"
+                    "public:"
+                    (->string class-name) "("
+                    "C_word proxy")
+              (map
+               (lambda (param)
+                 (string-append
+                  ", "
+                  (->string (caar param))
+                  " "
+                  (->string (cadr param))))
+               constructor-params)
+              (list
+               ") :"
+               (->string parent-class) "(")
+              (map
+               (lambda (params)
+                 (->string params))
+               parent-params)
+              (list
+               ")"
+               "{"
+               "	proxy_root = CHICKEN_new_gc_root();"
+               "	CHICKEN_gc_root_set(proxy_root, proxy);"
+               (->string (qt-proxy-callback-name
+                          (symbol-append
+                           class-name
+                           'constructor)))
+               "(proxy")
+              (map
+               (lambda (param)
+                 (string-append
+                  ", "
+                  (->string (cadr param))))
+               constructor-params)
+              (list
+               ");"
+               "}"
 
-         (qt-class
-          (,class-name ,parent-class)
-          (,constructor
-           self
-           ,@(map
-              (lambda (param)
-                (car param))
-              constructor-params)))))))
+               "~" (->string class-name) "()"
+               "{"
+               "CHICKEN_delete_gc_root(proxy_root);"
+               "}"
+
+               "};"))))
+
+          (qt-class
+           (,class-name ,parent-class)
+           (,constructor
+            self
+            ,@(map
+               (lambda (param)
+                 (car param))
+               constructor-params))))))))
 
 (define-syntax qt-app:exec-window!
   (lambda (e r c)
@@ -311,9 +397,16 @@
 (qt-proxy-class
  ImageWindow
  QMainWindow
- New-ImageWindow
- (((QWidget* c-pointer) parent #f))
- (parent))
+ (New-ImageWindow
+   (((QWidget* c-pointer) parent #f))
+   (lambda (self parent)
+     (print "Constructing!")))
+ (parent)
+ (lambda (self)
+   (print "Destructing!"))
+ ((SelectImage ((bool bool)) void
+               (lambda (self Selected?)
+                 (print Selected?)))))
 
 (qt-app:exec-window!
  MyImageWindow
